dd6cb84fef45878bcf1a783571626921d9c5f63b
Sergey Armodin <mak@arm1.ru>
2025-10-19 10:59:22 +0700

    feat: Add MainContextChannelExt module to utils for enhanced channel functionality

--- src/ui/utils/mod.rs
+++ src/ui/utils/mod.rs
@@ -1,5 +1,7 @@
+pub mod channel;
 pub mod rate_limit;
 pub mod workflow_run;
 
+pub use channel::MainContextChannelExt;
 pub use rate_limit::update_rate_limit_label;
 pub use workflow_run::{is_run_active, is_run_failure};
